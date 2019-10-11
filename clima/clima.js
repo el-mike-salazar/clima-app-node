0921f559ff4c9485ccd13a75120f750db7551999
Miguel <miguel.salazar@utags.edu.mx>
2019-10-10 22:14:19 -0500

    Reparado bug que siempre regresaba la misma temperatura II

--- clima/clima.js
+++ clima/clima.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
-var tuc = require('temp-units-conv');
 
 module.exports.getClima = async(lat, lon) => {
-    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=10080c685e28d4c418a6dfb5f10c49f1`);
-    return tuc.k2c(resp.data.main.temp);
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=10080c685e28d4c418a6dfb5f10c49f1`);
+    return resp.data.main.temp;
 }
